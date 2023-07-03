400bca91e89038fd305eb753bc9c19e331b0d88d
leonzh2k <leonzh2k@gmail.com>
2023-07-03 17:03:47 -0400

    refactor: migrate mcServices.js to TS

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,3 +4,18 @@ interface JournalStats {
   "Accept % | 1st R&R": number;
   "% Initial Submit => 1st R&R": number;
 }
+
+interface Manuscript {
+  manuscriptID: string;
+  initialSubmission?: boolean;
+  R1?: boolean;
+  R2?: boolean;
+  journal: string;
+  submission_date: string;
+  title: string;
+  decision: string;
+  terminalDecision?: true;
+  decisioned_date: string;
+  days: number;
+  journalFullName: string;
+}
