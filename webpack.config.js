fb10816cae7f7ca2b4df04a49ee0639931b24cca
leonzh2k <leonzh2k@gmail.com>
2023-07-05 20:48:02 -0400

    build: create webpack.config.js
    Create webpack.config.js. It tells webpack to:
    - Minify, uglify, and concatenate all content script files
      into one file into the build folder
    - Minify, uglify, and concatenate all background files
      into one file into the build folder
    - Copy src/manifest.json into the build folder

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  /* 
+    Two bundles, because chrome extensions require
+    content scripts and background scripts to be in 
+    separate files
+  */
+  entry: {
+    content: "./src/content.ts",
+    background: "./src/services/background.ts",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  watchOptions: {
+    poll: true, // Check for changes every second
+    ignored: /node_modules/,
+  },
+  plugins: [
+    // Copies manifest.json into /build every build
+    new CopyPlugin({
+      patterns: [{ from: "./src/manifest.json", to: "./manifest.json" }],
+    }),
+  ],
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "build"),
+    clean: true,
+  },
+};
