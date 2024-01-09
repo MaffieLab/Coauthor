c7fb6aac49155dc327dce0482d5c07ceaae7fd5d
leonzh2k <leonzh2k@gmail.com>
2024-01-09 15:23:20 -0800

    fix: stop dev errors being sent to prod logs

--- src/env.prod.ts
+++ src/env.prod.ts
@@ -1,5 +1,7 @@
 const env = {
   API_BASE_URL: "https://mctracker-api.fly.dev",
+  SENTRY_DSN:
+    "https://38c74fbfe4fb341db79a4c330e3e9357@o4505936942071808.ingest.sentry.io/4505959289454592",
 };
 
 export default env;
