a44d930ef41a9dd9bfcc87bfb2d9c9e2526d2a3a
leonzh2k <leonzh2k@gmail.com>
2024-02-03 14:45:40 -0800

    feat: remove toggling auth features via popup auth
    This feature is flaky and authentication via
    popup will be removed in a future commit, which
    means this feature will be removed anyway.

--- src/content.ts
+++ src/content.ts
@@ -314,22 +314,6 @@ const manuscriptUploadStatusColumn: {
         }
       }
     );
-    chrome.runtime.onMessage.addListener(async function (
-      message,
-      sender,
-      sendResponse
-    ) {
-      switch (message.type) {
-        case "addAuthenticatedFeatures":
-          manuscriptUploadStatusColumn.mount(manuscriptData);
-          sendResponse("success");
-          break;
-        case "removeAuthenticatedFeatures":
-          manuscriptUploadStatusColumn.unmount();
-          sendResponse("success");
-          break;
-      }
-    });
   } else if (submittedPage()) {
     addReviewTimeColumn();
   }
