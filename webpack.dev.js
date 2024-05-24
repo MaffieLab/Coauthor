3c57490d2f1a4899fe6e9ffb0f057047c7dc2eec
leonzh2k <leonzh2k@gmail.com>
2024-05-24 14:26:38 -0700

    build: rm unnecessary host perms from prod build

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,20 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 const webpack = require("webpack");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "development",
   devtool: "inline-source-map",
   plugins: [
+    // Copies manifest.json into /build every build
+    new CopyPlugin({
+      patterns: [
+        { from: "./src/manifest.json", to: "./manifest.json" },
+        { from: "./src/ui" },
+        { from: "./src/assets", to: "./assets" },
+      ],
+    }),
     new webpack.NormalModuleReplacementPlugin(
       /src[\\\/]env.ts/,
       "./env.dev.ts"
