b117cabb725838831488833bc6f0dd6650c522f2
leonzh2k <leonzh2k@gmail.com>
2024-09-27 21:16:04 -0700

    build: simplify manifest.json change process
    Simplify manifest build process by deleting the base manifest.json in
    /src, and instead completely generate the entire manifest.json at build
    time by writing generation code in the webpack config. This way devs
    only ever need to go to one place anytime they need to modify the
    manifest. For more context refer to issue #48

--- webpack.common.js
+++ webpack.common.js
@@ -2,6 +2,7 @@ const path = require("path");
 const { DefinePlugin } = require("webpack");
 const dotenv = require("dotenv");
 const CopyPlugin = require("copy-webpack-plugin");
+const fs = require("fs");
 
 // Load env vars that should be present in the final build.
 const publicEnvVars = dotenv.config({
@@ -11,16 +12,55 @@ const publicEnvVars = dotenv.config({
 // Load env vars that shouldn't be present in the final build but are required during the build process.
 dotenv.config({ path: `.env.${process.env.NODE_ENV}.private` });
 
-function modify(buffer) {
-  // copy-webpack-plugin passes a buffer
-  var manifest = JSON.parse(buffer.toString());
+const outputDir = path.resolve(__dirname, "build");
 
-  // Remove any host permissions not required in production
-  manifest.host_permissions = [`${process.env.API_BASE_URL}/*`];
+class BuildManifestPlugin {
+  constructor() {}
 
-  // pretty print to JSON with two spaces
-  manifest_JSON = JSON.stringify(manifest, null, 2);
-  return manifest_JSON;
+  apply(compiler) {
+    compiler.hooks.done.tap("BuildManifestPlugin", () => {
+      const outputFile = path.resolve(outputDir, "manifest.json");
+
+      const manifest = JSON.stringify(
+        {
+          manifest_version: 3,
+          name: "Coauthor",
+          version: JSON.parse(fs.readFileSync("./package.json")).version,
+          icons: {
+            128: "assets/icon128.png",
+          },
+          content_scripts: [
+            {
+              matches: ["https://mc.manuscriptcentral.com/*"],
+              run_at: "document_end",
+              js: ["content.js"],
+            },
+          ],
+          background: {
+            service_worker: "background.js",
+          },
+          action: {
+            default_popup: "./ui.html",
+          },
+          web_accessible_resources: [
+            {
+              resources: ["*.map", "assets/*.*"],
+              matches: ["<all_urls>"],
+            },
+          ],
+          description:
+            "Share your manuscript submission experiences and help increase transparency in academic publishing!",
+          permissions: ["identity"],
+          host_permissions: [`${process.env.API_BASE_URL}/*`],
+          key: "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA5EaziQp8N+0QkocGKkfq/AK6AgFpeGtJ+lblHfx3dqFpauY1z3himuj3y7j+jLLsQJdgOvDpzkleD5uB0uzN77tCixdt/7k509nKp/E9XCnFeFRPGC2GTnPndFqesjzvRHdxTlX3dHDTTkvHt1mpo/TXwVkkXtJvcG/ZCZTQFcw2/M7TeYr9iXbM0gcZc8OdwcPehDb+x6p1Spu6SyCiiYCfktMI8tKjSbp+bH/JRa+2iublrazzTkCx+bDvBM9N8AlmYaMVsgiqoaBWheb2bKkBVU91Q78QOMcwAXt1qpqFDcv0rtvsbDS6qcnwJTH/vxwOI6Zkr6kvEa2rt4+gBwIDAQAB",
+        },
+        null,
+        2
+      );
+
+      fs.writeFileSync(outputFile, manifest);
+    });
+  }
 }
 
 module.exports = {
@@ -43,6 +83,7 @@ module.exports = {
     ],
   },
   plugins: [
+    new BuildManifestPlugin(), // builds the manifest
     /**
      * This replaces all instances of 'process.env' in source code
      * with an object w/ environment variable key-value pairs. This means that
@@ -60,13 +101,6 @@ module.exports = {
 
     new CopyPlugin({
       patterns: [
-        {
-          from: "./src/manifest.json",
-          to: "./manifest.json",
-          transform(content, path) {
-            return modify(content);
-          },
-        },
         { from: "./src/ui" },
         { from: "./src/assets", to: "./assets" },
       ],
@@ -81,7 +115,7 @@ module.exports = {
   },
   output: {
     filename: "[name].js",
-    path: path.resolve(__dirname, "build"),
+    path: outputDir,
     clean: true,
   },
 };
